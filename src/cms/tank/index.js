ddc6f6cbbee0e0497b9920fcf072f45696f08b92
Hawk Kim <hawk.kim1974@gmail.com>
2019-04-09 15:11:44 +0900

    added multiple radar support with new configuration

--- src/cms/tank/index.js
+++ src/cms/tank/index.js
@@ -8,34 +8,139 @@ const vessel = require('../vessel');
  * update tank level
  * @param {number} ullageRadar - ullage from tank
  */
-function updateTankLevel(ullageRadar) {
-  const { cfg } = this;
-  const levelRadar = cfg.distance - ullageRadar;
-  const ullageSensor = ullageRadar + cfg.DVManMsrPntToSns;
+function updateTankLevel(tank) {
+  const t = tank;
+  let sumOfUllageAtRef = 0.0;
+  let sumOfUllageFC = 0.0;
+  let sumOfLevelAtRef = 0.0;
+  let sumOfLevelFC = 0.0;
+  let validRadarCount = 0;
 
-  this.ullage = ullageSensor - vessel.trimListCorrect(cfg.cDLFC, cfg.cDTFC);
-  this.ullage_floatation_center = ullageSensor - vessel.trimListCorrect(cfg.cDLFC, cfg.cDTFC);
-  this.level = levelRadar;
-  this.level_floatation_center = levelRadar
-    + vessel.trimListCorrect(cfg.cDLFC, cfg.cDTFC) - cfg.DBotManMsrPnt;
-  this.level_at_ref = levelRadar + vessel.trimListCorrect(cfg.cDLR, cfg.cDTR) - cfg.DBotManMsrPnt;
+  t.radars.forEach((r) => {
+    if (r.sensorFault === true) {
+      return;
+    }
+
+    sumOfUllageAtRef += r.ullageAtRef;
+    sumOfUllageFC += r.ullageFC;
+    sumOfLevelAtRef += r.levelAtRef;
+    sumOfLevelFC += r.levelFC;
+
+    validRadarCount += 1;
+  });
+
+  if (validRadarCount > 0) {
+    t.ullageAtRef = sumOfUllageAtRef / validRadarCount;
+    t.ullageFC = sumOfUllageFC / validRadarCount;
+    t.levelAtRef = sumOfLevelAtRef / validRadarCount;
+    t.levelFC = sumOfLevelFC / validRadarCount;
+  } else {
+    t.ullageAtRef = 0.0;
+    t.ullageFC = 0.0;
+    t.levelAtRef = 0.0;
+    t.levelFC = 0.0;
+  }
+}
+
+function updateRadar(radar, ullageRadar) {
+  const r = radar;
+  const { radarCfg, tank } = r;
+  const levelRadar = radarCfg.cfg.distance - ullageRadar;
+  const ullageSensor = ullageRadar + radarCfg.cfg.DVManMsrPntToSns;
+
+  r.ullageAtRef = ullageSensor - vessel.trimListCorrect(radarCfg.cfg.cDLR, radarCfg.cfg.cDTR);
+  r.ullageFC = ullageSensor - vessel.trimListCorrect(radarCfg.cfg.cDLFC, radarCfg.cfg.cDTFC);
+  r.level = levelRadar;
+  r.levelFC = levelRadar
+    + vessel.trimListCorrect(radarCfg.cfg.cDLFC, radarCfg.cfg.cDTFC) - radarCfg.cfg.DBotManMsrPnt;
+  r.levelAtRef = levelRadar
+    + vessel.trimListCorrect(radarCfg.cfg.cDLR, radarCfg.cfg.cDTR) - radarCfg.cfg.DBotManMsrPnt;
+
+  updateTankLevel(tank);
+}
+
+function createTankRadarInstance(tank, cfg) {
+  const radars = [];
+  let radar;
+
+  cfg.level.radars.forEach((radarCfg) => {
+    radar = {
+      tank,
+      sensorFault: false,
+      ullageAtRef: 0.0, // ullage at ref point
+      ullageFC: 0.0, // ullage at floatation center
+      levelFC: 0.0, // level at floatation center
+      level: 0.0, // level at sensor point
+      levelAtRef: 0.0, // level at ref point,
+      updateRadar,
+    };
+
+    radar.radarCfg = radarCfg;
+    radars.push(radar);
+  });
+  return radars;
 }
 
 /**
  * create tank object
- * @param {object} cfg - tank configuration object
+ * @param {object} cfg - tank configuration object with the following structure
+ cfg: {
+  name: 'XXX',
+  level: {
+    ullage: { // average ullage at reference point
+      channel: XXX,
+      hiAlarm: XXX,
+      loAlarm: XXX,
+    },
+    level: { // average level at reference point
+      channel: XXX,
+      hiAlarm: XXX,
+      loAlarm: XXX,
+    },
+    ullageAtFC: { // average ullage at floatation center
+      channel: XXX,
+      hiAlarm: XXX,
+      loAlarm: XXX,
+    },
+    levelAtFC: { // average level at floatation center
+      channel: XXX,
+      hiAlarm: XXX,
+      loAlarm: XXX,
+    },
+    tankLevelCfg: {
+      DVUllRefToUTI: XXX,
+      DBotUllRefPnt: XXX,
+    },
+    radars: [
+      {
+        channel: XXX,
+        sensorAlarm: XXX,
+        cfg: {
+          cDLR,
+          cDTR,
+          cDLFC,
+          cDTFC,
+          distance: XXX,
+          DBotManMsrPnt: XXX,
+          DVManMsrPntToSns: XXX,
+        },
+      },
+      ...
+    ],
+  },
+ }
  * @return {object} tank object
  */
 function createTank(cfg) {
   const tank = {
     cfg,
-    ullage: 0.0, // ullage at reference point
-    ullage_floatation_center: 0.0, // ullage at floatation center
-    level: 0.0, // level at sensor
-    level_floatation_center: 0.0, // level at floatation center
-    level_at_ref: 0.0, // level at reference point
-    updateTankLevel,
+    ullageAtRef: 0.0, // ullage at reference point
+    ullageFC: 0.0, // ullage at floatation center
+    levelAtRef: 0.0, // level at reference point
+    levelFC: 0.0, // level at floatation center
   };
+
+  tank.radars = createTankRadarInstance(tank, cfg);
 
   //
   // FIXME
--- test/tank.js
+++ test/tank.js
@@ -4,13 +4,105 @@ const vessel = require('../src/cms/vessel');
 
 const tankCfg = {
   name: 'sample tank',
-  distance: 10,
-  DVManMsrPntToSns: 0.0,
-  cDLFC: 0.0,
-  cDTFC: 0.0,
-  cDLR: 0.0,
-  cDTR: 0.0,
-  DBotManMsrPnt: 0.0
+  level: {
+    ullage: { // average ullage at reference point
+      channel: 1,
+      hiAlarm: 1,
+      loAlarm: 2,
+    },
+    level: { // average level at reference point
+      channel: 2,
+      hiAlarm: 3,
+      loAlarm: 4,
+    },
+    ullageAtFC: { // average ullage at floatation center
+      channel: 3,
+      hiAlarm: 5,
+      loAlarm: 6,
+    },
+    levelAtFC: { // average level at floatation center
+      channel: 4,
+      hiAlarm: 7,
+      loAlarm: 8,
+    },
+    tankLevelCfg: {
+      DVUllRefToUTI: 0,
+      DBotUllRefPnt: 0,
+    },
+    radars: [
+      {
+        channel: 10,
+        sensorAlarm: 10,
+        cfg: {
+          cDLR: 0.0,
+          cDTR: 0.0,
+          cDLFC: 0.0,
+          cDTFC: 0.0,
+          distance: 10,
+          DBotManMsrPnt: 0.0,
+          DVManMsrPntToSns: 0.0,
+        },
+      },
+    ],
+  },
+};
+
+const tankCfg2 = {
+  name: 'sample tank2',
+  level: {
+    ullage: { // average ullage at reference point
+      channel: 1,
+      hiAlarm: 1,
+      loAlarm: 2,
+    },
+    level: { // average level at reference point
+      channel: 2,
+      hiAlarm: 3,
+      loAlarm: 4,
+    },
+    ullageAtFC: { // average ullage at floatation center
+      channel: 3,
+      hiAlarm: 5,
+      loAlarm: 6,
+    },
+    levelAtFC: { // average level at floatation center
+      channel: 4,
+      hiAlarm: 7,
+      loAlarm: 8,
+    },
+    tankLevelCfg: {
+      DVUllRefToUTI: 0,
+      DBotUllRefPnt: 0,
+    },
+    radars: [
+      {
+        channel: 10,
+        sensorAlarm: 10,
+        cfg: {
+          cDLR: 0.0,
+          cDTR: 0.0,
+          cDLFC: 0.0,
+          cDTFC: 0.0,
+          distance: 10,
+          DBotManMsrPnt: 0.0,
+          DVManMsrPntToSns: 0.0,
+        },
+      },
+      {
+        channel: 11,
+        sensorAlarm: 11,
+        cfg: {
+          cDLR: 0.0,
+          cDTR: 0.0,
+          cDLFC: 0.0,
+          cDTFC: 0.0,
+          distance: 10,
+          DBotManMsrPnt: 0.0,
+          DVManMsrPntToSns: 0.0,
+        },
+      },
+    ],
+  },
 };
 
 describe('tank', () => {
@@ -19,11 +111,11 @@ describe('tank', () => {
     t = tank.createTank(tankCfg);
 
     assert.equal(t.cfg, tankCfg);
-    assert.equal(t.ullage, 0.0);
-    assert.equal(t.ullage_floatation_center, 0.0);
-    assert.equal(t.level, 0.0);
-    assert.equal(t.level_floatation_center, 0.0);
-    assert.equal(t.level_at_ref, 0.0);
+    assert.equal(t.radars.length, 1);
+    assert.equal(t.ullageAtRef, 0.0);
+    assert.equal(t.ullageFC, 0.0);
+    assert.equal(t.levelAtRef, 0.0);
+    assert.equal(t.levelFC, 0.0);
   });
 
   it('level update', () => {
@@ -32,19 +124,50 @@ describe('tank', () => {
 
     vessel.reset();
 
-    t.updateTankLevel(1.0);
-    assert.equal(t.ullage, 1.0);
-    assert.equal(t.ullage_floatation_center, 1.0);
-    assert.equal(t.level, 9.0);
-    assert.equal(t.level_floatation_center, 9.0);
-    assert.equal(t.level_at_ref, 9.0);
+    t.radars[0].updateRadar(t.radars[0], 1.0);
+    assert.equal(t.ullageAtRef, 1.0);
+    assert.equal(t.ullageFC, 1.0);
+    assert.equal(t.levelAtRef, 9.0);
+    assert.equal(t.levelFC, 9.0);
 
     vessel.updateTrimList(10, -10);
-    t.updateTankLevel(1.0);
-    assert.equal(t.ullage, 1.0);
-    assert.equal(t.ullage_floatation_center, 1.0);
-    assert.equal(t.level, 9.0);
-    assert.equal(t.level_floatation_center, 9.0);
-    assert.equal(t.level_at_ref, 9.0);
+    t.radars[0].updateRadar(t.radars[0], 1.0);
+    assert.equal(t.ullageAtRef, 1.0);
+    assert.equal(t.ullageFC, 1.0);
+    assert.equal(t.levelAtRef, 9.0);
+    assert.equal(t.levelFC, 9.0);
+  });
+
+  it('sensor fault', () => {
+    let t;
+    t = tank.createTank(tankCfg);
+
+    vessel.reset();
+
+    t.radars[0].sensorFault = true;
+    t.radars[0].updateRadar(t.radars[0], 1.0);
+    assert.equal(t.ullageAtRef, 0.0);
+    assert.equal(t.ullageFC, 0.0);
+    assert.equal(t.levelAtRef, 0.0);
+    assert.equal(t.levelFC, 0.0);
+  });
+
+  it('multi radars', () => {
+    let t;
+    t = tank.createTank(tankCfg2);
+
+    assert.equal(t.cfg, tankCfg2);
+    assert.equal(t.radars.length, 2);
+    assert.equal(t.ullageAtRef, 0.0);
+    assert.equal(t.ullageFC, 0.0);
+    assert.equal(t.levelAtRef, 0.0);
+    assert.equal(t.levelFC, 0.0);
+
+    t.radars[0].updateRadar(t.radars[0], 1.0);
+    t.radars[1].updateRadar(t.radars[1], 1.0);
+    assert.equal(t.ullageAtRef, 1.0);
+    assert.equal(t.ullageFC, 1.0);
+    assert.equal(t.levelAtRef, 9.0);
+    assert.equal(t.levelFC, 9.0);
   });
 });
