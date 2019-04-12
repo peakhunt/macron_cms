ab6fddf32f4a776019ed9ad171205e67d988199d
Hawk Kim <hawk.kim1974@gmail.com>
2019-04-12 11:25:31 +0900

    adding core and core test case

--- src/core/index.js
+++ src/core/index.js
@@ -1,7 +1,28 @@
 const alarm = require('./alarm');
 const channel = require('./channel');
 
+function init(channels, alarms) {
+  // create channels
+  for (let chnlNum in channels) {
+    let chnlCfg;
+
+    chnlCfg = channels[chnlNum];
+    channel.createChannel(chnlNum, chnlCfg);
+  }
+
+  // create alarms
+  for (let alarmNum in alarms) {
+    let alarmCfg;
+
+    alarmCfg = alarms[alarmNum];
+    alarm.createAlarm(alarmNum, alarmCfg);
+  }
+}
+
 module.exports = {
   alarm,
   channel,
+  init,
+  getAlarm: alarmNum => alarm.getAlarm(alarmNum),
+  getChannel: chnlNum => channel.getChannel(chnlNum),
 };
---
+++ test/core.js
@@ -0,0 +1,70 @@
+const assert = require('assert');
+const core = require('../src/core');
+
+const testChnlCfgs = {
+  100: {
+    name: 'Channel #1',
+    dir: 'in',
+    type: 'digital',
+  },
+  101: {
+    name: 'Channel #1',
+    dir: 'in',
+    type: 'analog',
+  },
+};
+
+const testAlarmCfgs = {
+  101: {
+    severity: 'minor',
+    name: 'test digital alarm on channel 10',
+    set: true,
+    delay: 0,
+    channel: 100,
+  },
+  102: {
+    severity: 'minor',
+    name: 'test digital alarm with delay on channel 10',
+    set: true,
+    delay: 1000,
+    channel: 100,
+  },
+  103: {
+    type: 'low',
+    severity: 'minor',
+    name: 'test low alarm on channel 11',
+    set: -10.0,
+    delay: 0,
+    channel: 101,
+  },
+  104: {
+    type: 'high',
+    severity: 'minor',
+    name: 'test high alarm on channel 11',
+    set: 10.0,
+    delay: 0,
+    channel: 101,
+  },
+  105: {
+    type: 'sensor_fault',
+    severity: 'minor',
+    name: 'test sensor fault alarm on channel 11',
+    delay: 0,
+    channel: 101,
+  },
+};
+
+describe('core', () => {
+  it('init', () => {
+    core.init(testChnlCfgs, testAlarmCfgs);
+
+    assert.notEqual(core.getChannel(100), undefined);
+    assert.notEqual(core.getChannel(101), undefined);
+
+    assert.notEqual(core.getAlarm(101), undefined);
+    assert.notEqual(core.getAlarm(102), undefined);
+    assert.notEqual(core.getAlarm(103), undefined);
+    assert.notEqual(core.getAlarm(104), undefined);
+    assert.notEqual(core.getAlarm(105), undefined);
+  });
+});
