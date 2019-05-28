5a5156f08189b878687966c2af9623c15288841c
Hawk Kim <hawk.kim1974@gmail.com>
2019-05-28 17:51:04 +0900

    working on modbus rtu slave

---
+++ src/io/modbusRTUSlave/index.js
@@ -0,0 +1,176 @@
+const core = require('../../core');
+const ModbusRTU = require('../../extensions/serverrtu');
+
+function ModbusRTUSlave(cfg) {
+  const self = this;
+  self.cfg = cfg;
+
+  const vector = {
+    /* eslint-disable no-unused-vars */
+    getInputRegister: (addr, unitID, cb) => {
+      const ioReg = self.cfg.registers.inputs[addr];
+
+      if (ioReg === undefined) {
+        cb({ modbusErrorCode: 0x02 });
+        return;
+      }
+
+      let value;
+
+      switch (ioReg.value) {
+        case 'value':
+          value = core.getChannel(ioReg.channel).sensorValue;
+          value = (value - ioReg.offset) / ioReg.gain;
+          break;
+
+        case 'status':
+          value = core.getChannel(ioReg.channel).sensorFault;
+          break;
+
+        case 'alarm':
+          value = core.getAlarm(ioReg.channel).state;
+          break;
+
+        default:
+          value = 0;
+          break;
+      }
+      cb(null, value);
+    },
+    /* eslint-disable no-unused-vars */
+    getHoldingRegister: (addr, unitID, cb) => {
+      const ioReg = self.cfg.registers.holdings[addr];
+
+      if (ioReg === undefined) {
+        cb({ modbusErrorCode: 0x02 });
+        return;
+      }
+
+      let value;
+
+      switch (ioReg.value) {
+        case 'value':
+          value = core.getChannel(ioReg.channel).sensorValue;
+          value = (value - ioReg.offset) / ioReg.gain;
+          break;
+
+        case 'status':
+          value = core.getChannel(ioReg.channel).sensorFault;
+          break;
+
+        case 'alarm':
+          value = core.getAlarm(ioReg.channel).state;
+          break;
+
+        default:
+          value = 0;
+          break;
+      }
+      cb(null, value);
+    },
+    /* eslint-disable no-unused-vars */
+    getCoil: (addr, unitID, cb) => {
+      const ioReg = self.cfg.registers.coils[addr];
+
+      if (ioReg === undefined) {
+        cb({ modbusErrorCode: 0x02 });
+        return;
+      }
+
+      let value;
+
+      switch (ioReg.value) {
+        case 'value':
+          value = core.getChannel(ioReg.channel).sensorValue;
+          break;
+
+        case 'status':
+          value = core.getChannel(ioReg.channel).sensorFault;
+          break;
+
+        default:
+          value = false;
+          break;
+      }
+      cb(null, value);
+    },
+    /* eslint-disable no-unused-vars */
+    getDiscreteInput: (addr, unitID, cb) => {
+      const ioReg = self.cfg.registers.coils[addr];
+
+      if (ioReg === undefined) {
+        cb({ modbusErrorCode: 0x02 });
+        return;
+      }
+
+      let value;
+
+      switch (ioReg.value) {
+        case 'value':
+          value = core.getChannel(ioReg.channel).sensorValue;
+          break;
+
+        case 'status':
+          value = core.getChannel(ioReg.channel).sensorFault;
+          break;
+
+        default:
+          value = false;
+          break;
+      }
+      cb(null, value);
+    },
+    /* eslint-disable no-unused-vars */
+    setRegister: (addr, value, unitID, cb) => {
+      const ioReg = self.cfg.registers.holdings[addr];
+
+      if (ioReg === undefined) {
+        cb({ modbusErrorCode: 0x02 });
+        return;
+      }
+
+      const v = value * ioReg.gain + ioReg.offset;
+
+      core.getChannel(ioReg.channel).sensorValue = v;
+
+      cb();
+    },
+    /* eslint-disable no-unused-vars */
+    setCoil: (addr, value, unitID, cb) => {
+      const ioReg = self.cfg.registers.coils[addr];
+
+      if (ioReg === undefined) {
+        cb({ modbusErrorCode: 0x02 });
+        return;
+      }
+
+      core.getChannel(ioReg.channel).sensorValue = value;
+      cb();
+    },
+  };
+
+  const options = {
+    unitID: cfg.address,
+    baudRate: cfg.transport.serial.baud,
+    dataBits: cfg.transport.serial.dataBit,
+    stopBits: cfg.transport.serial.stopBit,
+    parity: cfg.transport.serial.parity,
+  };
+
+  self.modbus = new ModbusRTU(vector, cfg.transport.serial.port, options);
+  self.started = false;
+
+  self.modbus.open(() => {
+    self.started = true;
+  });
+}
+
+function createModbusRTUSlave(cfg) {
+  const modbusRTUSlave = new ModbusRTUSlave(cfg);
+
+  return modbusRTUSlave;
+}
+
+module.exports = {
+  createModbusRTUSlave,
+};
