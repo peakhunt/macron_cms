f9c913d87de9a8386f8b061533a077fa68717bde
Hawk Kim <hawk.kim1974@gmail.com>
2019-05-28 09:49:31 +0900

    fixing various lint and bugs in serverrtu.js

--- extensions/serverrtu.js
+++ extensions/serverrtu.js
@@ -1,76 +1,78 @@
-"use strict";
-const util = require("util");
-const events = require("events");
+const util = require('util');
+const events = require('events');
+const SerialPort = require('serialport');
+const handlers = require('modbus-serial/servers/servertcp_handler');
+
 const EventEmitter = events.EventEmitter || events;
-const SerialPort = require("serialport");
-const modbusSerialDebug = require("debug")("modbus-serial");
 
 const UNIT_ID = 255;
 
+require('modbus-serial/utils/buffer_bit')();
 
-var handlers = require("./servertcp_handler");
-
-require("../utils/buffer_bit")();
-var crc16 = require("../utils/crc16");
+const crc16 = require('modbus-serial/utils/crc16');
 
 function _callbackFactory(unitID, functionCode, rtuWriter) {
+  let fc = functionCode;
+
   return function cb(err, responseBuffer) {
-    var crc;
+    let rspBuf = responseBuffer;
 
     // If we have an error.
     if (err) {
-      var errorCode = 0x04; // slave device failure
-      if (!isNaN(err.modbusErrorCode)) {
+      let errorCode = 0x04; // slave device failure
+      if (!Number.isNaN(err.modbusErrorCode)) {
         errorCode = err.modbusErrorCode;
       }
 
       // Set an error response
-      functionCode = parseInt(functionCode) | 0x80;
-      responseBuffer = Buffer.alloc(3 + 2);
-      responseBuffer.writeUInt8(errorCode, 2);
+      fc = parseInt(fc, 10) || 0x80;
+      rspBuf = Buffer.alloc(3 + 2);
+      rspBuf.writeUInt8(errorCode, 2);
     }
 
     // If we do not have a responseBuffer
-    if (!responseBuffer) {
-      return rtuWriter(null, responseBuffer);
+    if (!rspBuf) {
+      return rtuWriter(null, rspBuf);
     }
 
     // add unit number and function code
-    responseBuffer.writeUInt8(unitID, 0);
-    responseBuffer.writeUInt8(functionCode, 1);
+    rspBuf.writeUInt8(unitID, 0);
+    rspBuf.writeUInt8(fc, 1);
 
     // Add crc
-    crc = crc16(responseBuffer.slice(0, -2));
-    responseBuffer.writeUInt16LE(crc, responseBuffer.length - 2);
+    const crc = crc16(rspBuf.slice(0, -2));
+    rspBuf.writeUInt16LE(crc, rspBuf.length - 2);
 
     // Call callback function
-    return rtuWriter(null, responseBuffer);
+    return rtuWriter(null, rspBuf);
   };
 }
 
 function _parseModbusBuffer(reqFrame, vector, serverUnitID, rtuWriter) {
-  var cb;
-
   if (reqFrame.length < 4) {
     return;
   }
 
-  var unitID = reqFrame[0];
-  var functionCode = reqFrame[1];
-  var crc = reqFrame[reqFrame.length - 2] + reqFrame[reqFrame.length - 1] * 0x100;
+  const unitID = reqFrame[0];
+  let functionCode = reqFrame[1];
+  const crc = reqFrame[reqFrame.length - 2] + reqFrame[reqFrame.length - 1] * 0x100;
 
-  if (crc != crc16(reqFrame.slice(0, -2))) {
+  if (crc !== crc16(reqFrame.slice(0, -2))) {
     return;
   }
 
   if (serverUnitID !== 255 && serverUnitID !== unitID) {
+    /*
     console.log(` unit id check fail ${typeof serverUnitID} ${typeof unitID}`);
     console.log(` unit id check fail ${serverUnitID} ${unitID}`);
+    */
     return;
   }
-  cb = _callbackFactory(unitID, functionCode, rtuWriter);
+  const cb = _callbackFactory(unitID, functionCode, rtuWriter);
+  const errorCode = 0x01; // illegal function
+  let responseBuffer;
 
-  switch (parseInt(functionCode)) {
+  switch (parseInt(functionCode, 10)) {
     case 1:
     case 2:
       handlers.readCoilsOrInputDiscretes(reqFrame, vector, unitID, cb, functionCode);
@@ -97,40 +99,42 @@ function _parseModbusBuffer(reqFrame, vector, serverUnitID, rtuWriter) {
       handlers.handleMEI(reqFrame, vector, unitID, cb);
       break;
     default:
-      console.log('##### error case ####');
-      var errorCode = 0x01; // illegal function
+      // console.log('##### error case ####');
 
       // set an error response
-      functionCode = parseInt(functionCode) | 0x80;
-      var responseBuffer = Buffer.alloc(3 + 2);
+      functionCode = parseInt(functionCode, 10) || 0x80;
+
+      responseBuffer = Buffer.alloc(3 + 2);
+
       responseBuffer.writeUInt8(errorCode, 2);
       cb({ modbusErrorCode: errorCode }, responseBuffer);
   }
 }
 
-var ServerRTU = function(vector, path, options) {
-  var self = this;
+const ServerRTU = function serverRTU(vector, path, options) {
+  const self = this;
+  let opt = options;
 
   // options
-  if (typeof(options) === "undefined") options = {};
+  if (typeof opt === 'undefined') opt = {};
 
-  options.autoOpen = false;
+  opt.autoOpen = false;
 
-  var recvBuffer = Buffer.from([]);
-  const serverUnitID = options.unitID || UNIT_ID;
+  let recvBuffer = Buffer.from([]);
+  const serverUnitID = opt.unitID || UNIT_ID;
 
   // create the SerialPort
-  self._client = new SerialPort(path, options);
+  self._client = new SerialPort(path, opt);
   self._t35 = null;
 
-  self._client.on("data", function onData(data) {
+  self._client.on('data', (data) => {
     recvBuffer = Buffer.concat([recvBuffer, data], recvBuffer.length + data.length);
 
     if (self._t35 !== null) {
       clearTimeout(self._t15);
     }
 
-    var rtuWriter = function(err, responseBuffer) {
+    const rtuWriter = (err, responseBuffer) => {
       if (err) {
         return;
       }
@@ -143,7 +147,7 @@ var ServerRTU = function(vector, path, options) {
 
     // XXX 5ms timeout would be enough as T35 for most cases
     self._t35 = setTimeout(() => {
-      var reqFrame = Buffer.from([]);
+      let reqFrame = Buffer.from([]);
 
       // copy contents and reset
       reqFrame = recvBuffer.slice();
@@ -154,25 +158,25 @@ var ServerRTU = function(vector, path, options) {
           reqFrame,
           vector,
           serverUnitID,
-          rtuWriter), 0);
+          rtuWriter), 0,
+      );
     }, 5);
   });
 
-  Object.defineProperty(self, "isOpen", {
+  Object.defineProperty(self, 'isOpen', {
     enumerable: true,
-    get: function() {
-      return self._client.isOpen;
-    }
+    get: () => self._client.isOpen,
   });
   EventEmitter.call(self);
 };
+
 util.inherits(ServerRTU, EventEmitter);
 
-ServerRTU.prototype.open = function(callback) {
+ServerRTU.prototype.open = function open(callback) {
   this._client.open(callback);
 };
 
-ServerRTU.prototype.close = function(callback) {
+ServerRTU.prototype.close = function close(callback) {
   this._client.close(callback);
 };
 
