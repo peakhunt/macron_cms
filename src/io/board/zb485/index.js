7237f1eb236bf4c53dce91cabfb04a22c6230193
Hawk Kim <hawk.kim1974@gmail.com>
2019-06-27 17:15:44 +0900

    working on zb485

--- src/io/board/zb485/index.js
+++ src/io/board/zb485/index.js
@@ -1,9 +1,24 @@
+const ZB485StateEnum = {
+  Init: 0,
+  SensorTypeSetup: 1,
+  PowerSetup: 2,
+  ReadCommStatus: 3,
+  ExecuteANSGCNV: 4,
+};
+
+const ANSGCNVStateEnum = {
+  Init: 0,
+  ChannelSetup: 1,
+  ReadStatusInput: 2,
+};
+
 function ZB485(master, cfg) {
   const self = this;
 
   self.master = master;
   self.cfg = cfg;
   self.slaves = [];
+  self.state = ZB485StateEnum.Init;
 
   cfg.ports.forEach((pcfg, ndx) => {
     if (pcfg.use === true) {
@@ -12,6 +27,7 @@ function ZB485(master, cfg) {
 
       slave.cfg = pcfg;
       slave.port = (ndx + 1);
+      slave.state = ANSGCNVStateEnum.Init;
 
       self.slaves.push(slave);
     }
