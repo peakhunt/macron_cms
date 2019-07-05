e77e8bb1f973f365d3518b8a93a7235a0d01b42f
Hawk Kim <hawk.kim1974@gmail.com>
2019-07-05 16:49:40 +0900

    added app navigation drawer

--- cms_hmi/src/App.vue
+++ cms_hmi/src/App.vue
@@ -1,6 +1,32 @@
 <template>
   <v-app>
-    <v-toolbar app v-if="projectConfigLoaded">
+    <v-navigation-drawer
+     fixed
+     :mini-variant="miniVariant"
+     :clipped="clipped"
+     v-model="drawer"
+     app
+    >
+      <v-list>
+        <v-list-tile
+         router
+         :to="item.to"
+         :key="i"
+         v-for="(item, i) in items"
+         exact
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar app v-if="projectConfigLoaded" :clipped-left="clipped">
+      <v-toolbar-side-icon @click.native.stop="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title class="headline text-uppercase">
         <span>Vuetify</span>
         <span class="font-weight-light">MATERIAL DESIGN</span>
@@ -49,7 +75,18 @@ export default {
   },
   data() {
     return {
-      //
+      miniVariant: false,
+      clipped: true,
+      drawer: false,
+      items: [
+        { icon: 'apps', title: 'Welcome', to: '/' },
+        { icon: 'apps', title: 'Channel List', to: '/ChannelList' },
+        { icon: 'apps', title: 'Alarm List', to: '/AlarmList' },
+        { icon: 'apps', title: 'IO', to: '/IO' },
+        { icon: 'apps', title: 'Current Alarms', to: '/CurrentAlarms' },
+        { icon: 'apps', title: 'Tanks', to: '/TankList' },
+        { icon: 'apps', title: 'Vessel', to: '/Vessel' },
+      ],
     };
   },
 };
